21a04eb3099129d4b04e53bdb37d45b2d88d4436
kaysm <kaysm@rose-hulman.edu>
2019-01-22 15:04:53 -0500

    l

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,36 +2,44 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Steven Kay.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
-
+import rosegraphics as rg
+window = rg.TurtleWindow()
+window.tracer(100)
 ########################################################################
 #
-# TODO: 2.
+# Done: 2.
 #   Write code that accomplishes the following (and ONLY the following),
 #   in the order listed:
 #
 #    - Constructs a SimpleTurtle with a  'blue'  Pen.
 #
+blueboi = rg.SimpleTurtle('square')
+blueboi.pen = rg.Pen('blue', 3)
+point2 = rg.Point(0, 0)
+blueboi.go_to(point2)
 #    - Makes the SimpleTurtle go straight UP 200 pixels.
 #
+blueboi.left(90)
+blueboi.forward(200)
 #    - Makes the SimpleTurtle lift its pen UP
 #         (so that the next movements do NOT leave a "trail")
 #         HINT: Use the "dot trick" to figure out how to do this.
 #
+blueboi.pen_up()
 #    - Makes the SimpleTurtle go to the Point at (100, -40).
-#
-#    - Makes the SimpleTurtle put its pen DOWN
-#         (so that the next movements will return to leaving a "trail").
-#
-#    - Makes the SimpleTurtle's pen have color 'green' and thickness 10.
-#
-#    - Makes the SimpleTurtle go 150 pixels straight DOWN.
-#
+##
+point1 = rg.Point(100, -40)
+
+blueboi.go_to(point1)
+blueboi.pen_down()
+
+blueboi.forward(150)
 #   Don't forget to:
 #     - import rosegraphics and construct a TurtleWindow
 #          at the BEGINNING of your code, and to
@@ -43,3 +51,4 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+window.close_on_mouse_click()
