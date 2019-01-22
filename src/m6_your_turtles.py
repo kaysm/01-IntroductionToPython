934993304b42a2747090cc2b021b0a161e0e77c1
kaysm <kaysm@rose-hulman.edu>
2019-01-22 13:28:01 -0500

    l

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Steven Kay.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,28 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+window.tracer(100)
+blueboi = rg.SimpleTurtle('square')
+blueboi.pen = rg.Pen('blue', 3)
+blueboi.speed = 200
+for k in range(500):
+    blueboi.left(92)
+    blueboi.forward(k)
+redboi = rg.SimpleTurtle('square')
+redboi.pen = rg.Pen('red', 3)
+redboi.speed = 200
+for k in range(500):
+    redboi.right(92)
+    redboi.forward(k)
+greenboi = rg.SimpleTurtle('square')
+greenboi.pen = rg.Pen('green', 3)
+greenboi.speed = 200
+greenboi.backward(10)
+for k in range(500):
+    greenboi.backward(k)
+    greenboi.right(2)
+    greenboi.forward(k)
+
+window.close_on_mouse_click()
